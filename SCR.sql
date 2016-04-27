d1325669fd0579648e59ce56edee66abcb087861
mykryak <mykryak@gmail.com>
2016-04-27 08:33:59 +0300

    script structure BD -v2.1

--- SCR.sql
+++ SCR.sql
@@ -5,7 +5,7 @@ Discription: Table №1.
 create sequence vrsz_coordinc start with 1 increment by 1;
 CREATE TABLE  vrsz
   (
-    inc integer not null default nextval('vrsz_coordinc'),
+    inc integer not null default nextval('vrsz_coordinc') primary key,
     lat    real,
     long   real,
     datetime   timestamp,
@@ -13,14 +13,14 @@ CREATE TABLE  vrsz
     skor   real,
     ta   real,
     hw   real,
-    irain   real,
-    vrsz_meas integer
+    irain   real
   );
 /******************************************************************************/
-create sequence vrsz_measinc start with 1 increment by 1;
+--create sequence vrsz_measinc start with 1 increment by 1;
 CREATE TABLE  vrsz_meas
   (
-    inc integer not null default nextval('vrsz_measinc'),
+    --inc integer not null default nextval('vrsz_measinc'),
+    vrsz_coord integer references vrsz_coordinc(inc),
     nn    integer,
     hgor   real,
     zvuk   real
@@ -42,36 +42,37 @@ CREATE TABLE rmd
 Discription: Table №3.
 ------------------------------------------------------------------------------*/
 create sequence pgmd_coordinc start with 1 increment by 1;
-CREATE TABLE pgmd
+CREATE TABLE pgmd_coord
   (
-    inc integer NOT NULL DEFAULT nextval('pgmd_coordinc'::regclass),
+    inc integer NOT NULL DEFAULT nextval('pgmd_coordinc'::regclass) primary key,
     lat real,
     "long" real,
     datetime timestamp without time zone,
     ide_inf integer,
-    grnd integer,
-    refl integer,
+    grnd integer references grnd(name),
+    refl integer references refl(refl),
     ng integer
   );
 /******************************************************************************/
-create sequence grndinc start with 1 increment by 1;
+--create sequence grndinc start with 1 increment by 1;
 CREATE TABLE grnd
   (
-    inc integer NOT NULL DEFAULT nextval('grndinc'::regclass),
-    name character varying(50)
+    --inc integer NOT NULL DEFAULT nextval('grndinc'::regclass),
+    name character varying(50) primary key
   );
 /******************************************************************************/
-create sequence reflinc start with 1 increment by 1;
+--create sequence reflinc start with 1 increment by 1;
 CREATE TABLE refl
   (
-    inc integer NOT NULL DEFAULT nextval('reflinc'::regclass),
+    --inc integer NOT NULL DEFAULT nextval('reflinc'::regclass),
     refl real ARRAY[10]
   );
 /******************************************************************************/
-create sequence pgmd_measinc start with 1 increment by 1;
+--create sequence pgmd_measinc start with 1 increment by 1;
 CREATE TABLE pgmd_meas
   (
-    inc integer NOT NULL DEFAULT nextval('pgmd_measinc'::regclass),
+    --inc integer NOT NULL DEFAULT nextval('pgmd_measinc'::regclass),
+    pgmd_coord integer references pgmd_coord(inc),
     hsl real,
     zvuk real,
     alf real,
@@ -81,9 +82,9 @@ CREATE TABLE pgmd_meas
 Discription: Table №4.
 ------------------------------------------------------------------------------*/
 create sequence prgs_coordinc start with 1 increment by 1;
-CREATE TABLE  prgs
+CREATE TABLE  prgs_coord
      (
-       inc integer not null default nextval('prgs_coordinc'),
+       inc integer not null default nextval('prgs_coordinc') primary key,
        lat1    real,
        long1   real,
        lat2    real,
@@ -96,10 +97,11 @@ CREATE TABLE  prgs
        prgs_meas integer
      );
 /******************************************************************************/
-create sequence prgs_measinc start with 1 increment by 1;
+--create sequence prgs_measinc start with 1 increment by 1;
 CREATE TABLE  prgs_meas
      (
-       inc integer not null default nextval('prgs_measinc'),
+       --inc integer not null default nextval('prgs_measinc'),
+       prgs_coord integer references prgs_coord(inc),
        datetime timestamp,
        dist   real,
        hlzl    real,
@@ -110,9 +112,9 @@ CREATE TABLE  prgs_meas
 Discription: Table №5.
 ------------------------------------------------------------------------------*/
 create sequence chan_coordinc start with 1 increment by 1;
-CREATE TABLE  chan
+CREATE TABLE  chan_coord
   (
-    inc integer not null default nextval('chan_coordinc'),
+    inc integer not null default nextval('chan_coordinc') primary key,
     lat    real,
     long   real,
     hgor   real,
@@ -126,10 +128,11 @@ CREATE TABLE  chan
     chan_meas integer
   );
 /******************************************************************************/
-create sequence chan_measinc start with 1 increment by 1;
+--create sequence chan_measinc start with 1 increment by 1;
 CREATE TABLE  chan_meas
   (
-    inc integer not null default nextval('chan_measinc'),
+    --inc integer not null default nextval('chan_measinc'),
+    chan_coord integer references chan_coord(inc),
     nn    integer,
     hgor   real,
     frec   real,
@@ -139,9 +142,9 @@ CREATE TABLE  chan_meas
 Discription: Table №6.
 ------------------------------------------------------------------------------*/
 create sequence pm_coordinc start with 1 increment by 1;
-CREATE TABLE  pm
+CREATE TABLE  pm_coord
   (
-    inc integer not null default nextval('pm_coordinc'),
+    inc integer not null default nextval('pm_coordinc') primary key,
     lat    real,
     long   real,
     datetime   timestamp,
@@ -154,9 +157,9 @@ CREATE TABLE  pm
 Discription: Table №7.
 ------------------------------------------------------------------------------*/
 create sequence ht_coordinc start with 1 increment by 1;
-CREATE TABLE  ht
+CREATE TABLE  ht_coord
   (
-    inc integer not null default nextval('ht_coordinc'),
+    inc integer not null default nextval('ht_coordinc') primary key,
     lat    real,
     long   real,
     datetimebeg   timestamp,
@@ -168,10 +171,11 @@ CREATE TABLE  ht
     ht_meas integer
   );
   /****************************************************************************/
-create sequence ht_measinc start with 1 increment by 1;
+--create sequence ht_measinc start with 1 increment by 1;
 CREATE TABLE  ht_meas
     (
-      inc integer not null default nextval('ht_measinc'),
+      --inc integer not null default nextval('ht_measinc'),
+      ht_coord integer references ht_coord(inc),
       nn    integer,
       hgor   real,
       vstream   real,
