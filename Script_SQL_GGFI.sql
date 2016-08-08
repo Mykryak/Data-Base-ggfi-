93d8b5bfea562814297a4c53d9e8fa2eaacaf708
mykryak <mykryak@gmail.com>
2016-08-08 15:04:04 +0300

    Fixed sixth table

--- Script_SQL_GGFI.sql
+++ Script_SQL_GGFI.sql
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------------------
  Create Author: Artem Gavrilov M.
- Version: 3.1.6
+ Version: 3.2.0
  Discription: SQL-запросы для создания таблиц БД ГГФИ
 ------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------
@@ -16,8 +16,7 @@ CREATE DATABASE ggfi_beta
   CONNECTION LIMIT = -1;
 /******************************************************************************/
 CREATE sequence ide_inf_seq start with 1 increment by 1;
-CREATE TABLE ide_inf
-(
+CREATE TABLE ide_inf (
   inc integer NOT NULL DEFAULT nextval('ide_inf_seq'::regclass),
   name character varying(255),
   CONSTRAINT ide_inf_pkey PRIMARY KEY (inc )
@@ -26,8 +25,7 @@ CREATE TABLE ide_inf
  Discription: Table №1.
 ------------------------------------------------------------------------------*/
 CREATE sequence vrsz_coordinc start with 1 increment by 1;
-CREATE TABLE vrsz_coord
-(
+CREATE TABLE vrsz_coord (
   inc integer NOT NULL DEFAULT nextval('vrsz_coordinc'::regclass),
   lat real,
   "long" real,
@@ -42,23 +40,21 @@ CREATE TABLE vrsz_coord
   CONSTRAINT vrsz_coord_ide_inf_fkey FOREIGN KEY (ide_inf)
       REFERENCES ide_inf (inc) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
-);
+  );
 /******************************************************************************/
-CREATE TABLE vrsz_meas
-(
+CREATE TABLE vrsz_meas (
   vrsz_coord integer,
   hgor real,
   zvuk real,
   CONSTRAINT vrsz_meas_vrsz_coord_fkey FOREIGN KEY (vrsz_coord)
       REFERENCES vrsz_coord (inc) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
-);
+    );
 /*------------------------------------------------------------------------------
 -Discription: Table №2.
 ------------------------------------------------------------------------------*/
 CREATE sequence rmd_coordinc start with 1 increment by 1;
-CREATE TABLE rmd
-  (
+CREATE TABLE rmd (
     inc integer NOT NULL DEFAULT nextval('rmd_coordinc'::regclass),
     lat real,
     "long" real,
@@ -73,22 +69,19 @@ CREATE TABLE rmd
 -Discription: Table №3.
 ------------------------------------------------------------------------------*/
 CREATE sequence grndinc start with 1 increment by 1;
-CREATE TABLE grnd
-  (
+CREATE TABLE grnd (
     inc integer NOT NULL DEFAULT nextval('grndinc'::regclass) primary key,
     name character varying(50)
   );
 /******************************************************************************/
 CREATE sequence reflinc start with 1 increment by 1;
-CREATE TABLE refl
-  (
+CREATE TABLE refl (
     inc integer NOT NULL DEFAULT nextval('reflinc'::regclass) primary key,
     refl real ARRAY[10]
   );
 /******************************************************************************/
 create sequence pgmd_coordinc start with 1 increment by 1;
-CREATE TABLE pgmd_coord
-(
+CREATE TABLE pgmd_coord (
   inc integer NOT NULL DEFAULT nextval('pgmd_coordinc'::regclass),
   lat real,
   "long" real,
@@ -109,8 +102,7 @@ CREATE TABLE pgmd_coord
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 /******************************************************************************/
-CREATE TABLE pgmd_meas
-(
+CREATE TABLE pgmd_meas (
   pgmd_coord integer,
   hsl real,
   zvuk real,
@@ -124,8 +116,7 @@ CREATE TABLE pgmd_meas
 -Discription: Table №4.
 ------------------------------------------------------------------------------*/
 CREATE sequence prgs_coordinc start with 1 increment by 1;
-CREATE TABLE prgs_coord
-(
+CREATE TABLE prgs_coord (
   inc integer NOT NULL DEFAULT nextval('prgs_coordinc'::regclass),
   lat1 real,
   long1 real,
@@ -139,8 +130,7 @@ CREATE TABLE prgs_coord
   CONSTRAINT prgs_coord_pkey PRIMARY KEY (inc)
 );
 /******************************************************************************/
-CREATE TABLE prgs_meas
-(
+CREATE TABLE prgs_meas (
   prgs_coord integer,
   dist real,
   hlzl real,
@@ -154,8 +144,7 @@ CREATE TABLE prgs_meas
 -Discription: Table №5.
 ------------------------------------------------------------------------------*/
 CREATE sequence chan_coordinc start with 1 increment by 1;
-CREATE TABLE chan_coord
-(
+CREATE TABLE chan_coord (
   inc integer NOT NULL DEFAULT nextval('chan_coordinc'::regclass),
   lat real,
   "long" real,
@@ -171,8 +160,7 @@ CREATE TABLE chan_coord
   CONSTRAINT chan_coord_pkey PRIMARY KEY (inc)
 );
 /******************************************************************************/
-CREATE TABLE chan_meas
-(
+CREATE TABLE chan_meas (
   chan_coord integer,
   frec real,
   pnois real,
@@ -183,24 +171,65 @@ CREATE TABLE chan_meas
 /*------------------------------------------------------------------------------
 -Discription: Table №6.
 ------------------------------------------------------------------------------*/
-CREATE sequence pm_coordinc start with 1 increment by 1;
-CREATE TABLE  pm_coord
-  (
-    inc integer not null default nextval('pm_coordinc') primary key,
-    lat    real,
-    long   real,
-    datetime   timestamp,
-    ns   real,
-    nw   real,
-    nr   real,
-    pmet   real
+CREATE TABLE pm_coord (
+  inc       serial NOT NULL,
+  lat       real,
+  long      real,
+  datetime  date,
+  CONSTRAINT pm_coord_pkey
+    PRIMARY KEY (inc)
+);
+/******************************************************************************/
+CREATE TABLE pm_coord_nr (
+  inc_nr  serial NOT NULL,
+  diap    varchar(30) NOT NULL,
+  CONSTRAINT pm_coord_nr_pkey
+    PRIMARY KEY (inc_nr)
+);
+/******************************************************************************/
+CREATE TABLE pm_coord_nw (
+  inc_nw  serial NOT NULL,
+  diap    varchar(30) NOT NULL,
+  CONSTRAINT pm_coord_nw_pkey
+    PRIMARY KEY (inc_nw)
+);
+/******************************************************************************/
+CREATE TABLE pm_coord_ns (
+  inc_ns  serial NOT NULL,
+  diap    varchar(30) NOT NULL,
+  CONSTRAINT pm_coord_ns_pkey
+    PRIMARY KEY (inc_ns)
+);
+CREATE TABLE pm_coord_pmet (
+  inc_pmet  serial NOT NULL,
+  inc       integer NOT NULL,
+  inc_ns    integer,
+  inc_nw    integer,
+  inc_nr    integer NOT NULL,
+  pmet      real,
+  CONSTRAINT pm_coord_pmet_pkey PRIMARY KEY (inc_pmet),
+  CONSTRAINT foreign_key01 FOREIGN KEY (inc)
+    REFERENCES public.pm_coord(inc)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT foreign_key02 FOREIGN KEY (inc_ns)
+    REFERENCES public.pm_coord_ns(inc_ns)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT foreign_key03 FOREIGN KEY (inc_nw)
+    REFERENCES public.pm_coord_nw(inc_nw)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT foreign_key04 FOREIGN KEY (inc_nr)
+    REFERENCES public.pm_coord_nr(inc_nr)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 /*------------------------------------------------------------------------------
 -Discription: Table №7.
 ------------------------------------------------------------------------------*/
 CREATE sequence ht_coordinc start with 1 increment by 1;
-CREATE TABLE ht_coord
-(
+CREATE TABLE ht_coord (
   inc integer NOT NULL DEFAULT nextval('ht_coordinc'::regclass),
   lat real,
   "long" real,
@@ -218,8 +247,7 @@ CREATE TABLE ht_coord
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 /******************************************************************************/
-CREATE TABLE ht_meas
-(
+CREATE TABLE ht_meas (
   ht_coord integer,
   hgor real,
   vstream real,
